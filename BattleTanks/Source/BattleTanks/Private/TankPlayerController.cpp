c657b31a5350865a34817522fe7a35862bfaa0cd
DESKTOP-L4VMUFS\Chris <chrisjokim@gmail.com>
2017-07-26 17:40:49 -0400

    Creating and Assigning Tank Controllers

---
+++ BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay(); //Super says call the default behavior before anything else.
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Not possessing tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possessing: %s"), *(ControlledTank->GetName())); //The "*" gets the contents of the location here. You get the location and * gets the contents
+	}
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"))
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
