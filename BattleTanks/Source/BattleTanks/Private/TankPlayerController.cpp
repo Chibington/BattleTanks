93a50859f725ea28236dd1e4ae184b443ec4c7a7
DESKTOP-L4VMUFS\Chris <chrisjokim@gmail.com>
2017-07-28 22:01:44 -0400

    LineTracing and HitResults on Terrain

--- BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
@@ -1,6 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "Engine/World.h" 
+
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
 
 void ATankPlayerController::BeginPlay()
 {
@@ -18,9 +26,84 @@ void ATankPlayerController::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
 }
 
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+
+	FVector OutHitLocation; //Out parameter
+	if (GetSightRayHitLocation(OutHitLocation)) //Has Side-effect which is going to the sightrayhit. The reference to the FVector& for OutHit allows the OutHitLocation to be referencing the OutHit 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *OutHitLocation.ToString());
+			// TODO Tell controlled tank to aim at the point
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHit) const //Fvector& OutHit is a reference to the OutHitLocation above
+{
+	// Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	//Finds the crosshair location in terms of your own screensize
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX*CrosshairXLocation, ViewportSizeY*CrosshairYLocation);
+	//UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString());
+
+	//De-project the screen position of the crosshair to a world direction
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		//Line-trace along the look direction and see what he hit up to a max range
+		GetLookVectorHitLocation(LookDirection, OutHit);
+		
+	}
+
+
+	return true;
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+	if (GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECollisionChannel::ECC_Visibility)//Tells us to hit anything that is visible
+		)
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	return false; //Line trace didn't succeed
+}
+
+
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const //The stuff in the parenthesis is the stuff you plug into this function. They both derive from the function above
+{
+	//De-project the screen position of the crosshair to a world direction
+	FVector CameraWorldLocation;//To be discarded
+
+	return	DeprojectScreenPositionToWorld(
+		ScreenLocation.X,
+		ScreenLocation.Y,
+		CameraWorldLocation,
+		LookDirection);
+}
+
 
 
--- BattleTanks/Source/BattleTanks/Public/TankPlayerController.h
+++ BattleTanks/Source/BattleTanks/Public/TankPlayerController.h
@@ -16,7 +16,32 @@ class BATTLETANKS_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-	
+	virtual void Tick(float DeltaTime) override;
+
 	virtual void BeginPlay() override; //Virtual here means nothing but in a higher up inheritance, adding virtual allows the method to be accessed or overridden by it's children
+
+	ATank* GetControlledTank() const;
+
+	
+
+private:
+	//Start the tank moving the barrel so that a shot would hit where the crosshair intersects with the world
+	void AimTowardsCrosshair(); 
+	// Returns an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const; //Return an OUT parameter, true if it hits landscape
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairXLocation = .5f;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairYLocation = .33333f;
+
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 1000000.f;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+
+
 };
